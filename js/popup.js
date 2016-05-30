5a4a909c3405eb0f7c285cfd423dec219b8ad9d7
teddyzhu <teddyzhu15@gmail.com>
2016-05-30 18:08:06 +0800

    add web ui for popup

--- js/popup.js
+++ js/popup.js
@@ -1,8 +1,35 @@
 $(function() {
-    var editorJavascript = ace.edit("edJS");
+    var editorJavascript = ace.edit("edJS"),
+        editorCSS = ace.edit("edCSS"),
+        editUrl = $('#url'),
+        originUrl;
     editorJavascript.setTheme("ace/theme/eclipse");
     editorJavascript.getSession().setMode("ace/mode/javascript");
-    var editorCSS = ace.edit("edCSS");
+    editorJavascript.setOptions({
+        enableBasicAutocompletion: true,
+        enableLiveAutocompletion: true,
+        enableSnippets: true
+    });
     editorCSS.setTheme("ace/theme/eclipse");
     editorCSS.getSession().setMode("ace/mode/css");
+    chrome.tabs.query({
+            'active': true,
+            'windowId': chrome.windows.WINDOW_ID_CURRENT
+        },
+        function(tabs) {
+            originUrl = tabs[0].url;
+            editUrl.val(originUrl);
+        }
+    );
+    $('#save').on('click', function() {
+        console.log(editorJavascript.getValue());
+        console.log(editorCSS.getValue());
+    })
+
+    $('#reset').on('click', function() {
+        editorJavascript.setValue("");
+        editorCSS.setValue("");
+        editUrl.val(originUrl);
+    })
+
 })
